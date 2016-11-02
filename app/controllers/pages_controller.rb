58d3347f05e658ac8a6262471e611638c8707bc8
Tyler <tcameronsmith@gmail.com>
2016-11-02 15:22:59 -0400

    headers added to welcome, about, and contest pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,14 +1,14 @@
 class PagesController < ApplicationController
 
 	def welcome
-
+		@header = "this is the welcome page boo ya"
 	end
 
 	def about
-
+		@header = "this aint welcome page, its an about"
 	end
 
 	def contest
-
+		@header = "step right up, see what youll probably not win.."
 	end
 end
