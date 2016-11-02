cda5fd4660479a779c46b23c790d963cb70930b1
Tyler <tcameronsmith@gmail.com>
2016-11-02 15:32:09 -0400

    create route and controller for kitten/size page via dynamic link

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,6 +8,11 @@ class PagesController < ApplicationController
 		@header = "this aint welcome page, its an about"
 	end
 
+	def kitten
+		request_size = params[:size]
+		@kitten_url = "http://placekitten.com/#{request_size}"
+	end
+
 	def contest
 		@header = "step right up, see what youll probably not win.."
 	end
